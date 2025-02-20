f0b797d4b51dac88d7d21d22c97b0a90d263e425
AceAsWall <aceaswall@gmail.com>
2025-02-20 15:24:05 -0300

    Create questionNine.py

---
+++ questionNine.py
@@ -0,0 +1,12 @@
+# This is a question from the book "Automate The Boring Stuff" (Question 9 - Chapter 2)
+spam = 0
+
+while spam < 10:
+    if spam == 1:
+        print('Hello')
+    elif spam == 2:
+        print('Howdy')
+    elif spam > 2:
+        print('Greetings!')
+
+    spam += 1
