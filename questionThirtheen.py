2507f8f8f1dcf8e1c3c526a00bb4003f944e8b53
AceAsWall <aceaswall@gmail.com>
2025-02-20 18:09:36 -0300

    Create questionThirtheen.py

---
+++ questionThirtheen.py
@@ -0,0 +1,12 @@
+# This is a question from the book "Automate The Boring Stuff" (Question 13 - Chapter 2)
+
+#13. Write a short program that prints the numbers 1 to 10 using a for loop. Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+# for
+#for i in range(0, 11):
+#    print(i)
+
+n = 0
+while n < 11:
+    print(n)
+    n = n + 1
